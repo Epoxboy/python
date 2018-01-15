7da516659d2668ee11cca6dee152384b1fe6b4c0
JASON LI <li.jason.aus@gmail.com>
2018-01-16 08:55:09 +1100

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -3,11 +3,6 @@
 # this is a demo
 
 import boto3
-
-ec2 = boto3.resource('ec2')
-for instance in ec2.instances.all():
-    print instance.id, instance.state
-
     
 s3 = boto3.resource('s3')
 for bucket in s3.buckets.all():
